03202021afca616bf2ac9e1e7bb301dd8cb76e85
사삭이 <85689028+sasak2@users.noreply.github.com>
2022-11-27 09:30:58 +0000

    all files are here

--- .ccls-cache/@home@runner@BOJ/main.cpp
+++ .ccls-cache/@home@runner@BOJ/main.cpp
@@ -1,6 +1,23 @@
 #include<iostream>
+#include<vector>
+#include<algorithm>
 
+using namespace std;
 int main() {
-  printf("Heasdfllo, Wasdfaewforld!");
+	freopen("input.txt","rt",stdin);
+
+	int n;
+	int a;
+	vector<int> v;
+	
+	scanf("%d",&n);
+	
+	for(int i=0; i<n;i++) {
+		scanf("%d",&a);
+		v.push_back(a);
+	}
+	sort(v.begin(),v.end());
+	for(int i=0; i<n;i++)
+		printf("%d\n",v[i]);
   return 0;
 }
