5c15f4cd073b026b516b8e03c1c655f22dd02933
사삭이 <85689028+sasak2@users.noreply.github.com>
2022-11-30 06:04:13 +0000

    1085 AC

--- .ccls-cache/@home@runner@BOJ/main.cpp
+++ .ccls-cache/@home@runner@BOJ/main.cpp
@@ -2,37 +2,55 @@
 #include<vector>
 #include<queue>
 #include<algorithm>
+#include<cstring>
+#include<cmath>
 
 using namespace std;
+
+//1018 체스판 다시 칠하기
+
+int n,m;
+char ch[52][52];
+
+int fill(int x, int y, char wb) {
+
+	int d=0;
+	int cnt=0;;
+	for(int i=x; i<x+8; i++) {
+		for(int k=y; k<y+8; k++) {
+			if((ch[i][k]==wb)==d)
+				cnt++;
+			d++;
+			d%=2;
+			
+			
+		}
+	}
+
+	return cnt;
+}
+
 int main() {
 	freopen("input.txt","rt",stdin);
 
-	int n;
-	char ch[30];
+	scanf("%d %d",&n,&m);
+	int min_=999;
 
-	scanf
-	while(1) {
-		scanf("%d %d %d",&a,&b,&c);
-		if(a+b+c==0)
-			break;
-		if(a>c) {
-	  	temp=c;
-	  	c=a;
-	  	a=temp;
-  	}
-	  if(b>c) {
-		  temp=c;
-	  	c=b;
-	  	b=temp;
-	  }
-		if(c*c==(a*a)+(b*b))
-			printf("right\n");
-		else
-			printf("wrong\n");
-		
+	for(int i=0; i<n; i++) 
+		scanf("%s",ch[i]);
+
+	for(int i=0; i<=n-8; i++) {
+		for(int k=0; k<=m-8; k++) {
+			int b=fill(i,k,'B');
+			int a=fill(i,k,'W');
+			if(min_>a)
+				min_=a;
+			if(min_>b)
+				min_=b;
+		}
 	}
 
-	
-	
-  return 0;
+	printf("%d",min_);
+
+	return 0;
 }
