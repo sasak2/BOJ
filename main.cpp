7ee6d5f6d0ebb20cf0af2643339494b03009bcda
sasak2 <poocon0818@naver.com>
2022-12-16 11:30:42 +0900

    Main

--- main.cpp
+++ main.cpp
@@ -9,53 +9,15 @@
 
 using namespace std;
 
-//15657 N과M 8
+//14939 불 끄기
 
-int n,m;
-int arr[10];
-int num[10];
 
-void print(void) {
-	for(int i=0; i<m; i++) {
-		printf("%d ",num[arr[i+1]-1]);
-	}
-	printf("\n");
-}
 
-void f(int depth) {
-	if(depth==m+1)  {
-		print();
-		return;
-		}
-	
-
-	for(int i=arr[depth-1]; i<=n; i++) {
-	
-	
-		arr[depth]=i;
-		f(depth+1);
-
-		
-	}
- 
-	
-	
-}
 
 int main() {
 	freopen("input.txt","rt",stdin);
 
-	scanf("%d %d",&n,&m);
-
-	for(int i=0; i<n; i++)
-		scanf("%d",&num[i]);
-
-	sort(num,num+n);
-	
-	arr[0]=1;
-	f(1);
 
 	
-
 	return 0;
 }
