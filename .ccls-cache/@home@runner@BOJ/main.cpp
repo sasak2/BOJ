f249431e46d72bfa151e50655561527e25fb93b1
사삭이 <85689028+sasak2@users.noreply.github.com>
2022-12-16 03:29:06 +0000

    4619 AC

--- .ccls-cache/@home@runner@BOJ/main.cpp
+++ .ccls-cache/@home@runner@BOJ/main.cpp
@@ -9,33 +9,27 @@
 
 using namespace std;
 
-//11053 가장 긴 증가하는 부분 수열
+//4619 루트
 
-int n;
-int arr[1004];
-int big[1004]={0,};
+int b,n;
 
 int main() {
 	freopen("input.txt","rt",stdin);
 
-	scanf("%d",&n);
-	for(int i=0; i<n; i++) 
-		scanf("%d",&arr[i]);
-
-	for(int i=0; i<n; i++) {
-		int d=0;
-		for(int k=0; k<i; k++) {
-			if(arr[k]<arr[i]&&big[k]>=d)
-				d=big[k];
-		}
-		big[i]=d+1;
+	while(1) {
+		scanf("%d %d",&b,&n);
+		if(n==0)
+			break;
+		
+		int sum = pow(double(b),1.0/n);
+		int mindiff = sum;
+		if(abs(pow(mindiff,n)-b) > abs(pow(sum+1,n)-b))
+			mindiff=sum+1;
+		if(abs(pow(mindiff,n)-b) > abs(pow(sum-1,n)-b))
+			mindiff=sum-1;
+		printf("%d\n",mindiff);
 	}
+	
 
-	int wow=1;
-	for(int i=0; i<n; i++) {
-		if(big[i]>wow)
-			wow=big[i];
-	}
-	printf("%d",wow);
 	return 0;
 }
