8d48d09ee4c454d7dbd5d8978a9efd7b8d908b5a
사삭이 <85689028+sasak2@users.noreply.github.com>
2023-01-29 23:00:09 +0900

    [Gold V] Title: 평범한 배낭, Time: 28 ms, Memory: 41476 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+ 
+int dp[101][100001];
+int w[101];
+int v[101];
+int n, k;
+ 
+int main()
+{
+    cin >> n >> k;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> w[i] >> v[i];
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= k; j++)
+        {
+      
+            if (j >= w[i])
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+     
+            else
+                dp[i][j] = dp[i - 1][j];
+        }
+    }
+    cout << dp[n][k];
+    return 0;
+}
