152d5f91439ba8614d63f15a4314b90398ab5c54
사삭이 <85689028+sasak2@users.noreply.github.com>
2023-01-29 22:53:14 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int a,b;
+    scanf("%d %d",&a,&b);
+
+    if(a==b)
+        printf("==");
+    else if(a>b)
+        printf(">");
+    else
+        printf("<");
+    
+    return 0;
+}
